7acfba82715dc0d98375635ff16c7f47a99851bb
James Kim <jamesjkim8806@gmail.com>
2016-03-17 22:49:05 -0700

    code for ui.router

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,40 @@
-var app = angular.module("myApp", [ui.router]);
+var app = angular.module("myApp", ["ui.router"]);
+
+app.config(function($stateProvider, $urlRouterProvider){
+    $stateProvider.state("about", {
+        url: "/",
+        templateUrl: "../templates/_about.html",
+        controller: "aboutCtrl"
+    }).state("portfolio", {
+        url: "/portfolio",
+        templateUrl: "../templates/_portfolio.html",
+        controller: "portfolioCtrl"
+    }).state("contact", {
+        url: "/contact",
+        templateUrl: "../templates/_contact.html",
+        controller: "contactCtrl"
+    });
+    $urlRouterProvider.otherwise("/");
+});
+
+app.controller("aboutCtrl", function($scope){
+    $scope.message = "about";
+});
+
+app.controller("portfolioCtrl", function($scope){
+    $scope.message = "portfolio";
+});
+
+app.controller("contactCtrl", function($scope){
+    $scope.message = "contact";
+});
 
 app.controller("mainCtrl", function($scope, flickr){
     $scope.message = "Hello";
 });
 
-app.factory("flickr", function($http){
-    return{
-
-    }
-});
+//app.factory("flickr", function($http){
+//    return{
+//
+//    }
+//});
